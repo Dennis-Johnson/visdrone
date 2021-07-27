6edaa384cb6464d069211c960e8b47bcddd975e1
Dennis Johnson <densarjohn@gmail.com>
2021-07-27 23:12:37 +0530

    Cleaned up utils

--- convertVis_to_xml.py
+++ convertVis_to_xml.py
@@ -1,19 +1,20 @@
+'''
+Convert Visdrone annotation txt file to XML representation
+Visdrone Annotation Format:
+<bbox_left>, <bbox_top>, <bbox_width>, <bbox_height>, <score>, <object_category>, <truncation>, <occlusion>
+'''
+
 import cv2
 import os
-import numpy as np
 
 
-input_img_folder = './visdrone_val/images'             #Path to input images folder
-input_ann_folder = './visdrone_val/annotations'       	#Path to original text annotations folder
-output_ann_folder = './annotations_val'               #Path to output annotaions in xml format
-output_img_folder = './images_val'          #Path to ouput images with the bounding boxes shown
+input_img_folder = 'visdrone_val/images'            #Path to input images folder
+input_ann_folder = 'visdrone_val/annotations'       #Path to original text annotations folder
+output_ann_folder = 'annotations_val'            #Path to output annotaions in xml format
+output_img_folder = 'images_val'          		#Path to ouput images with the bounding boxes shown
 
 os.makedirs(output_img_folder, exist_ok=True)
 os.makedirs(output_ann_folder, exist_ok=True)
-
-
-image_list = os.listdir(input_img_folder)
-annotation_list = os.listdir(input_ann_folder)
 
 label_dict = {
 	"0": "Ignored Regions",
@@ -30,12 +31,13 @@ label_dict = {
 	"11" : "Others",
 }
 
-thickness = 2
+# Bounding Box thickness and color
+thickness = 1
 color = (255,0,0)
-count = 0
 
-def object_string(label, bbox):
-	req_str = '''
+
+def constructObjectSection(label, bbox):
+	return '''
 	<object>
 		<name>{}</name>
 		<pose>Unspecified</pose>
@@ -49,19 +51,21 @@ def object_string(label, bbox):
 		</bndbox>
 	</object>
 	'''.format(label, bbox[0], bbox[1], bbox[2], bbox[3])
-	return req_str
 
-for annotation in annotation_list:
-	annotation_path = os.path.join(os.getcwd(), input_ann_folder, annotation)
-	xml_annotation = annotation.split('.txt')[0] + '.xml'
-	xml_path = os.path.join(os.getcwd(), output_ann_folder, xml_annotation)
 
-	img_file = annotation.split('.txt')[0] + '.jpg'
-	img_path = os.path.join(os.getcwd(), input_img_folder, img_file)
-	
-	output_img_path = os.path.join(os.getcwd(), output_img_folder, img_file)
+for count, annotation_file in enumerate(os.listdir(input_ann_folder)):
+	annotation_path = os.path.join(os.getcwd(), input_ann_folder, annotation_file)
+
+	xml_annotation_name = annotation_file.split('.txt')[0] + '.xml'
+	xml_path = os.path.join(os.getcwd(), output_ann_folder, xml_annotation_name)
+
+	img_file_name = annotation_file.split('.txt')[0] + '.jpg'
+	img_path = os.path.join(os.getcwd(), input_img_folder, img_file_name)
+	output_img_path = os.path.join(os.getcwd(), output_img_folder, img_file_name)
+
 	img = cv2.imread(img_path)
-	annotation_string_init = '''
+
+	annotation_string = '''
 <annotation>
 	<folder>annotations</folder>
 	<filename>{}</filename>
@@ -74,23 +78,32 @@ for annotation in annotation_list:
 		<height>{}</height>
 		<depth>{}</depth>
 	</size>
-	<segmented>0</segmented>'''.format(img_file, img_path, img.shape[0], img.shape[1], img.shape[2])
+	<segmented>0</segmented>'''.format(img_file_name, img_path, img.shape[0], img.shape[1], img.shape[2])
 
 	file = open(annotation_path, 'r')
-	lines = file.readlines()
-	for line in lines:
-		new_line = line.strip('\n').split(',')
-		new_coords_min = (int(new_line[0]), int(new_line[1]))
-		new_coords_max = (int(new_line[0])+int(new_line[2]), int(new_line[1])+int(new_line[3]))
-		bbox = (int(new_line[0]), int(new_line[1]), int(new_line[0])+int(new_line[2]), int(new_line[1])+int(new_line[3]))
-		label = label_dict.get(new_line[5])
-		req_str = object_string(label, bbox)
-		annotation_string_init = annotation_string_init + req_str
-		cv2.rectangle(img, new_coords_min, new_coords_max, color, thickness)
+
+	for line in file.readlines():
+		# Cast all elements to ints
+		line = [int(i) for i in line.strip('\n').split(',')] 
+
+		coords_top_left  = (line[0],  line[1])
+		coords_top_right = (line[0] + line[2], line[1] + line[3])
+		bbox = (*coords_top_left, *coords_top_right)
+		
+		# Get category
+		label = label_dict.get(line[5])			
+		
+		# Append a single object's details
+		objectSection = constructObjectSection(label, bbox)
+		annotation_string += objectSection 
+
+		cv2.rectangle(img, coords_top_left, coords_top_right, color, thickness)
+
 	cv2.imwrite(output_img_path, img)
-	annotation_string_final = annotation_string_init + '</annotation>'
+	annotation_string +=  '</annotation>'
+
 	f = open(xml_path, 'w')
-	f.write(annotation_string_final)
+	f.write(annotation_string)
 	f.close()
 	count += 1
 	print('[INFO] Completed {} image(s) and annotation(s) pair'.format(count))
--- rename_files.py
+++ rename_files.py
@@ -1,21 +1,18 @@
-# Pythono3 code to rename multiple 
-# files in a directory or folder 
+'''
+Util for Visdrone Dataset
+Renames images (jpg) and annotations (txt)to consecutive integers.
 
-# importing os module 
+Input: 
+'''
+
 import os 
 
-annotations_path = '/Users/dennis/development/visdrone/visdrone_val'
-# Function to rename multiple files 
-def main(): 
+path = '/Users/dennis/development/visdrone/visdrone_val/annotations/'
+file_extension = '.txt'
 
-	for count, filename in enumerate(os.listdir("D:\\8th SEM project\\vis_new\\train_annotations_txt\\")): #Directory in which the files have to be renamed
-		dst =str(count) + ".txt"
-		src ='D:\\8th SEM project\\vis_new\\train_annotations_txt\\'+ filename #original file name
-		dst ='D:\\8th SEM project\\vis_new\\train_annotations_txt\\'+ dst  #new file name
-		
-		os.rename(src, dst) 
-
-if __name__ == '__main__': 
-
-	main() 
-	print("Files have been renamed successfully.")
+for count, filename in enumerate(os.listdir(path)):
+	src = path + filename
+	dst = path + str(count) + file_extension
+	os.rename(src, dst) 
+	
+print("Files renamed")
